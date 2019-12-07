0147de76127993f84393ccd06202a20e3745679a
ChanakaA <chanakaathurugiriya@gmail.com>
2019-12-07 18:24:02 +0530

    update regex and enable dynamic update

--- plugin/index.js
+++ plugin/index.js
@@ -13,15 +13,9 @@ module.exports = function ({ types: t }) {
         name: 'babel-plugin-rn-white-label',
         visitor: {
             CallExpression (path, state) {
-                const callee = path.get("callee");
-                const _arguments = path.get("arguments");
-
-                if (callee.type === 'Identifier'
-                && callee.name === 'require'
-                && _arguments.length) {
-
-                    // skip if calee is an MemberExpression
-                    if (!callee.isMemberExpression()) return;
+                if (path.node.callee.type === 'Identifier'
+                    && path.node.callee.name === 'require'
+                    && path.node.arguments.length) {
 
                     // skip if in paths to ignore
                     if (default_paths_to_ignore.find(_path => (state.file.opts.filename.indexOf(_path) > -1)))  return;
@@ -34,15 +28,15 @@ module.exports = function ({ types: t }) {
                     // skip if options are not specified
                     if (!options.mask || !options.exts.length) return;
                    
-                    const regx = new RegExp(`((\.)(.*))?(\.)(${options.exts.join('|')})$`);
-                    const file_name = _arguments[0].value.replace(/^(\.\/)/, '');
+                    const regx = new RegExp(`((\\.)([^\/]*))?(\\.)(${options.exts.join('|')})$`);
+                    const file_name = path.node.arguments[0].value.replace(/^(\.\/)/, '');
 
                     const hits = regx.exec(file_name);
 
                     // skip if given extension doesn't match
-                    if (!hits || hits.length) return;
+                    if (!hits || !hits.length) return;
 
-                    if (hits[2] === options.mask) {
+                    if (hits[3] === options.mask) {
                         const file_dir_org = state.file.opts.filename.replace(/\/([^\/\s]*)$/, '/') + file_name;
                         
                         // skip if original requirement satisfied
@@ -52,16 +46,16 @@ module.exports = function ({ types: t }) {
                         
                         // replace if file found without mask extension
                         if (existsSync(file_dir_without_mask)) {
-                            path.replaceWith(t.callExpression(path.node.callee, [t.stringLiteral(_arguments[0].value.replace(regx, `$4$5`))]));
+                            path.replaceWith(t.callExpression(path.node.callee, [t.stringLiteral(path.node.arguments[0].value.replace(regx, `$4$5`))]));
                             path.skip();
                         }
                     }
                     else {
-                        const file_dir = state.file.opts.filename.replace(/\/([^\/\s]*)$/, '/') + file_name.replace(regx, `$1${options.mask}.$2`);
+                        const file_dir = state.file.opts.filename.replace(/\/([^\/\s]*)$/, '/') + file_name.replace(regx, `$1$4${options.mask}$4$5`);
 
                         // modify require statement if required assets found
                         if (existsSync(file_dir)) {
-                            path.replaceWith(t.callExpression(path.node.callee, [t.stringLiteral(_arguments[0].value.replace(regx, `$1${options.mask}.$2`))]));
+                            path.replaceWith(t.callExpression(path.node.callee, [t.stringLiteral(path.node.arguments[0].value.replace(regx, `$1$4${options.mask}$4$5`))]));
                             path.skip();
                         }
                     }
